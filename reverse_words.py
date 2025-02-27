10640df0892062a545b91a629303ea8150401ee2
princeVegeta2 <nellatorbeaufort@gmail.com>
2025-02-27 17:16:26 +0100

    Reverses words

---
+++ reverse_words.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    # Description:
+    # Reverse words in a given string
+    # Return reversed string
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words = s.split()
+        words.reverse()
+        new_s = ""
+        for i in range(len(words)):
+            if i == len(words) - 1:
+                new_s += words[i]
+            else:
+                new_s += words[i]
+                new_s += " "
+
+
+        return new_s
+    
+    # Leetcode best
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        New = s.split()
+        Val= New[::-1]
+        return ' '.join(Val)
+    
+    
+sol = Solution()
+print(sol.reverseWords("  hello world  "))
